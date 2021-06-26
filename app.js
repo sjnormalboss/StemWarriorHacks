fe132e50019b128da87aa50a65df3f7538e31b3c
supus19 <saaketh2127@gmail.com>
2021-06-26 12:34:22 -0700

    image

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const header = document.querySelector('.header.container');
 
+
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
@@ -15,3 +16,4 @@ document.addEventListener('scroll', ()=>{
         header.style.backgroundColor=" transparent";
     }
 })
+
