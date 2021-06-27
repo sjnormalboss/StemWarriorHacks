512a490077c00a279d183c49e0a5a3cf89291bc2
supus19 <saaketh2127@gmail.com>
2021-06-26 17:47:13 -0700

    Events added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const header = document.querySelector('.header.container');
 
 
+
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
