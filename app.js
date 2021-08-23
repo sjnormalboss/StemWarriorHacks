99c55fcc25093ab959189f3e6b0417b259331258
giridhar7632 <giridhar.talla2002@gmail.com>
2021-08-23 11:31:32 +0530

    tiny changes

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const header = document.querySelector('.header.container');
-const faqs = document.querySelectorAll(".faq");
+const hamburger = document.querySelector(
+	'.header .nav-bar .nav-list .hamburger'
+)
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul')
+const header = document.querySelector('.header.container')
+const faqs = document.querySelectorAll('.faq')
 
-
-
-hamburger.addEventListener('click', ()=>{
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active')
+	mobile_menu.classList.toggle('active')
 })
 
-document.addEventListener('scroll', ()=>{
-    var scroll_position = window.scrollY;
-    if(scroll_position > 250) {
-        header.style.backgroundColor = '#29323c';
-    } else {
-        header.style.backgroundColor=" transparent";
-    }
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#262626'
+	} else {
+		header.style.backgroundColor = ' transparent'
+	}
 })
 
-
-faqs.forEach(faq => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("active");
-    })
+faqs.forEach((faq) => {
+	faq.addEventListener('click', () => {
+		faq.classList.toggle('active')
+	})
 })
